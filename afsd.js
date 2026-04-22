fbf487055c6db05bc2d097df0d163d48d269a397
umar <umarzokirov911@gmail.com>
2026-04-22 14:24:48 +0500

    fs

--- afsd.js
+++ afsd.js
@@ -25,3 +25,18 @@ for (let i = 0; i < foods.length; i++) {
 for (let food of foods) {
     console.log(food);
 }
+
+
+let arr = [1, 2, 3, 4, 5,34,64253,68];
+arr.sort()
+// arr.reverse()
+console.log(arr);
+
+
+let arr2 = [1,2,4,5]
+arr2.forEach(function(element,index) {
+    console.log(`Element: ${element}, Index: ${index}`)
+
+    arr2[index] = element * 2
+});
+console.log(arr2);
